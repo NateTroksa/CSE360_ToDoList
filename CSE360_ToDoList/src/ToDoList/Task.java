ce88c3688667bca10353221b776019664766d226
shivam2345 <32351363+shivam2345@users.noreply.github.com>
2019-04-24 12:58:31 -0700

    Javadoc comments to Task

--- CSE360_ToDoList/src/ToDoList/Task.java
+++ CSE360_ToDoList/src/ToDoList/Task.java
@@ -1,10 +1,8 @@
-
 package ToDoList;
-
-import java.util.Date;
         
 public class Task {
     
+	// global variabels
     private int priority = 0;
     private String description = " ";
     private String status = " ";
@@ -14,6 +12,10 @@ public class Task {
     
     public static String newline = System.getProperty("line.separator");
     
+    /**
+     * Constructor for the Task Class 
+     * @param int priority, String status, String dueDate, String description, String startDate, String completedDate
+     * @return void*/
     public Task(int priority, String status, String dueDate, String description, String startDate, String completedDate){
         this.priority = priority;
         this.status = status;
@@ -23,25 +25,91 @@ public class Task {
         this.completedDate = completedDate;
     }
     
+    /**
+     * Accessor Method to get Task objects priority
+     * @param void
+     * @return priority*/
     public int getPriority(){ return priority; }
+    
+    /**
+     * Accessor Method to get Task objects status
+     * @param void
+     * @return status*/
     public String getStatus(){ return status; }
+    
+    /**
+     * Accessor Method to get Task objects dueDate
+     * @param void
+     * @return dueDate*/
     public String getDueDate(){ return dueDate; }
+    
+    /**
+     * Accessor Method to get Task objects description
+     * @param void
+     * @return description*/
     public String getDescription(){ return description; }
+    
+    /**
+     * Accessor Method to get Task objects startDate
+     * @param void
+     * @return startDate*/
     public String getStartDate(){ return startDate; }
+    
+    /**
+     * Accessor Method to get Task objects completeDate
+     * @param void
+     * @return completeDate*/
     public String getCompletedDate(){ return completedDate; }
     
+    /**
+     * Mutator Method to set Task objects priority
+     * @param priority
+     * @return void*/
     public void setPriority(int priority){ this.priority = priority; }
+    
+    /**
+     * Mutator Method to set Task objects status
+     * @param status
+     * @return void*/
     public void setStatus(String status){ this.status = status; }
+    
+    /**
+     * Mutator Method to set Task objects dueDate
+     * @param dueDate
+     * @return void*/
     public void setDueDate(String dueDate){ this.dueDate = dueDate; }
+    
+    /**
+     * Mutator Method to set Task objects description
+     * @param description
+     * @return void*/
     public void setDescription(String description){ this.description = description; }
+    
+    /**
+     * Mutator Method to set Task objects startDate
+     * @param startDate
+     * @return void*/
     public void setStartDate(String startDate){ this.startDate = startDate; }
+    
+    /**
+     * Mutator Method to set Task objects completedDate
+     * @param completedDate
+     * @return void*/
     public void setCompletedDate(String completedDate){ this.completedDate = completedDate; }
     
+    /**
+     * toString Method for Task Object
+     * @param void
+     * @return String[] array*/
     public String[] toStringArray(){
         String s[] = {Integer.toString(priority), status, dueDate, description, startDate, completedDate};
         return s;
     }
     
+    /**
+     * toString Method for Task value
+     * @param void
+     * @return String[] array*/
     public String toString(){
         String s = "";
         s += "Priority: " + priority + newline;
